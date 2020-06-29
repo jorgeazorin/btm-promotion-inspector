c3f50ac897faed898f9751df62caa1180763fcd0
jorgeazorin <jorgeazorin@gmail.com>
2020-06-29 15:08:00 +0200

    Updates

---
+++ precache-manifest.ba179e03733cb5c4e72c97a8df2eafea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b5c47de9fb3c884094b2db20c94508",
+    "url": "/btm-promotion-inspector/index.html"
+  },
+  {
+    "revision": "10abfce5c21b608900b5",
+    "url": "/btm-promotion-inspector/static/css/main.8b32e31b.chunk.css"
+  },
+  {
+    "revision": "c814e7b8ee913172b74e",
+    "url": "/btm-promotion-inspector/static/js/2.e4e52de9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/btm-promotion-inspector/static/js/2.e4e52de9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10abfce5c21b608900b5",
+    "url": "/btm-promotion-inspector/static/js/main.972603a7.chunk.js"
+  },
+  {
+    "revision": "628409305a323569728f",
+    "url": "/btm-promotion-inspector/static/js/runtime-main.4c44975e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/btm-promotion-inspector/static/media/logo.5d5d9eef.svg"
+  }
+]);
